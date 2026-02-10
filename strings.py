4cbc81809470ca913ccdce4bb1e66e99f0d066b6
Ashley Namu <namuashley@gmail.com>
2026-02-10 10:37:23 +0300

    Learn string variables and basic string methods

---
+++ strings.py
@@ -0,0 +1,16 @@
+first_name = "Ashley"
+last_name = "Namu"
+
+full_name = first_name + " " + last_name
+
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Full name:", full_name)
+
+message = "Welcome to Python programming"
+print(message)
+
+print("Uppercase:", message.upper())
+print("Lowercase:", message.lower())
+print("Length of message:", len(message))
+
